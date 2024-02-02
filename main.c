27958c4c6c0a8ec7add83659a66f3bb0f730829e
Thyago-Oliveira-Perez <thyagodoliveiraperez@gmail.com>
2024-02-01 22:03:06 -0300

    feat: add menu() function

--- main.c
+++ main.c
@@ -9,8 +9,32 @@ void read_file(char *file_location)
   printf("Content:\n%s\n", buffer);
 }
 
+int menu()
+{
+  printf("Choose an option:\n");
+  printf("1. Read file\n");
+  printf("2. Edit file\n");
+  printf("3. Append file\n");
+  printf("0. Exit\n");
+  printf(":");
+  int option;
+  scanf("%d", &option);
+  return option;
+}
+
 void main(int argc, char **argv)
 {
   char *file_location = argv[1];
-  read_file(file_location);
+
+  int option = menu();
+
+  if (option == 0)
+  {
+    return;
+  }
+
+  if (option == 1)
+  {
+    read_file(file_location);
+  }
 }
